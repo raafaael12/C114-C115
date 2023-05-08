fe78022a1f62eaec7aad231451a305ecfafd7ab9
raafaael12 <111808805+raafaael12@users.noreply.github.com>
2023-05-08 18:32:54 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,33 @@
+var noseX = 0;
+var noseY = 0;
+
+
+function preload(){
+mouth = loadImage('https://i.postimg.cc/bwjjRb45/biquinho-removebg-preview.png')
+}
+function setup(){
+canvas = createCanvas(300,300);
+canvas.center();
+video = createCapture(VIDEO);
+video.size(300,300);
+video.hide();
+
+poseNet = ml5.poseNet(video, modelLoaded);
+poseNet.on('pose', gotPoses)
+}
+function modelLoaded(){
+    console.log("poseNet foi iniciado")
+}
+function gotPoses(results){
+if (results.length > 0){
+noseX = results[0].pose.nose.x - 20;
+noseY = results[0].pose.nose.y +10;
+}
+}
+function draw(){
+image(video, 0,0, 300, 300);
+image(mouth, noseX, noseY, 40, 30)
+}
+function takeSnapshot(){
+save('myFilterImage.png')
+}
